b4f6895252baaf937c76850fc8521eab02ca025b
A9-dev <henrypearson422@gmail.com>
2025-04-23 14:01:49 +0100

    feat(ui): :sparkles: edited layout and customised nav bar

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -35,20 +35,16 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+            <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16 bg-black/10">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <HeaderAuth />
                 </div>
               </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
-              </div>
+            <div className="flex flex-col gap-20 max-w-5xl p-5">{children}</div>
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
                 Footer stuff here
               </footer>
-            </div>
           </main>
         </ThemeProvider>
       </body>
