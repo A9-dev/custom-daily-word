d4e4bcd9b562803ea9f7d314e34172e85dd4bd2d
A9-dev <henrypearson422@gmail.com>
2025-04-23 14:03:39 +0100

    chore(frontend): update metadata for site

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -10,8 +10,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Custom Word of the Day",
+  description: "Discover a new personalised word of the day",
 };
 
 const geistSans = Geist({
@@ -36,15 +36,15 @@ export default function RootLayout({
         >
           <main className="min-h-screen flex flex-col items-center">
             <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16 bg-black/10">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <HeaderAuth />
-                </div>
-              </nav>
+              <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                <HeaderAuth />
+              </div>
+            </nav>
             <div className="flex flex-col gap-20 max-w-5xl p-5">{children}</div>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                Footer stuff here
-              </footer>
+            <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+              Footer stuff here
+            </footer>
           </main>
         </ThemeProvider>
       </body>
